6575f695e3226cc1b0e0118dd33aa2079518887f
MrSpring521 <143806544+MrSpring521@users.noreply.github.com>
2023-09-01 21:21:18 +0800

    Create determine_parity.c

---
+++ determine_parity.c
@@ -0,0 +1,12 @@
+int main()
+{
+	int num;
+	scanf("%d",  &num);
+	if (num % 2 == 1)
+		printf("是奇数\n");
+	else
+	    printf("是偶数\n");
+	return 0;
+}
+//判断输入的数的奇偶性，用于分支语句的练习
+//Determine the parity of the input number, used for branch statement practice
